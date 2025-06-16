6fb7638355d14e3f2f201b89481e1b11538e4222
NellitG <kamaunelson157@gmail.com>
2025-06-16 19:04:34 +0300

    Initial setup of vue template

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -19,19 +19,19 @@ const routes = [
 /**Initialize here */
 const router = createRouter({ history: createWebHistory(), routes });
 
-router.beforeEach(async (to, from) => {
-  // redirect to login page if not logged in and trying to access a restricted page
-  const publicPages = ["/", "/login"];
-  const authRequired = !publicPages.includes(to.path);
-  const auth = useAuthStore();
+// router.beforeEach(async (to, from) => {
+//   // redirect to login page if not logged in and trying to access a restricted page
+//   const publicPages = ["/", "/login"];
+//   const authRequired = !publicPages.includes(to.path);
+//   const auth = useAuthStore();
 
-  /**add function to check if user is logged in */
-  if (authRequired && !auth.user) {
-    auth.returnUrl = to.fullPath;
-    auth.isLoginModalOpen = true;
-    return from.fullPath;
-    // return "/login";
-  }
-});
+//   /**add function to check if user is logged in */
+//   if (authRequired && !auth.user) {
+//     auth.returnUrl = to.fullPath;
+//     auth.isLoginModalOpen = true;
+//     return from.fullPath;
+//     // return "/login";
+//   }
+// });
 
 export default router;
